faf676ccd90a7adca5c311b6eea246b4159f8a6e
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2023-12-10 11:52:48 +0100

    move application folder to [comercio](https://github.com/Priceless-P/comercio)

--- applications/comercio/product_service/application/product_api/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-product_api_blueprint = Blueprint('product_api', __name__)
-
-from application.product_api import routes
--- applications/comercio/user_service/application/user_api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Defines User api blueprint"""
-
-from flask import Blueprint
-
-user_api_blueprint = Blueprint('user_api', __name__,)
-
-from application.user_api import routes
